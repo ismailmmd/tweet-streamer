aa7e33f08f1c80a7476e1c49d863ebf721b55523
ismail <37075892+ismail5701@users.noreply.github.com>
2020-09-26 01:39:06 +0530

    git iniy

---
+++ src/main/java/com/demo/TweetStreamer.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetStreamer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetStreamer.class, args);
+	}
+
+}
